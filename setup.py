4691bf1ce9d44521609771e5e66018a45022244d
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2024-04-02 12:41:40 +0800

    Update version to 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="MLstatkit",
-    version="0.1.2",
+    version="0.1.3",
     description="MLstatkit is a comprehensive Python library designed to seamlessly integrate established statistical methods into machine learning projects.",
     long_description=long_description,
     long_description_content_type="text/markdown",
